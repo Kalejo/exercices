2ada336eb93b030d8be72fc3cc859f5802effcc7
Kalejo <alejo_alvarez67@hotmail.com>
2018-04-10 23:15:40 +0200

    exo2

--- exo2.rb
+++ exo2.rb
@@ -2,12 +2,11 @@
 def chiffre_de_cesar(string, shift = 1)
   alphabet  = Array('a'..'z')
   non_caps  = Hash[alphabet.zip(alphabet.rotate(shift))]
-  
+
   alphabet = Array('A'..'Z')
   caps     = Hash[alphabet.zip(alphabet.rotate(shift))]
-  
+
   encrypter = non_caps.merge(caps)
-  
   string.chars.map { |c| encrypter.fetch(c, c) }
 end
 
@@ -22,5 +21,13 @@ end
 
 p caesar_cipher("testing").join
 =end
+=begin
 def chiffre_de_cesar (Lettre + Clef %26)
 	alphabet = []
+=end
+def chiffre_de_cesar (string, shift = 3)
+	alphabet = Array("a".."z")
+	encrypter = Hash[alphabet.zip(alphabet.rotate(shift))]
+	string.chars.map { |c| encrypter.fetch(c,"")}
+end
+puts chiffre_de_cesar("what a strting").join
