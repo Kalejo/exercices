f563b089f841e8f4c61ca4a9b99ddab316e42bd6
Kalejo <alejo_alvarez67@hotmail.com>
2018-04-10 17:32:59 +0200

    exercices

---
+++ exo1.rb
@@ -0,0 +1,61 @@
+=begin
+resultat = 0
+for i in 1..1000
+	if i %3 == 0 || i%5 == 0
+		resultat += i
+	end
+	puts resultat
+end
+=end
+=begin
+def multiple (a, b, c, d)
+	return a + b + c + d
+end
+	puts multiple (3 + 5 + 6 + 9)
+=end
+
+=begin
+def multiple (3, 5, 6, 9)
+	sum = 23
+	numbre
+=end
+
+
+=begin
+
+def sum (*nb)
+	if nb == []
+	return = 23
+
+end
+
+def sum (nb =[])
+	if nb == []
+		return = 1000
+	end
+end
+=end
+
+def sum
+	total = 0
+	1000.times do |i|
+		if  i %3 == 0 || i %5 == 0
+			total += i
+		end
+	end
+	puts total
+end
+sum
+
+=begin
+def sum
+total = 0
+	for i in 1..1000
+		if i %3 == 0 || i %5 == 0
+		total += i
+		end
+	end
+	puts total
+end
+sum
+=end
